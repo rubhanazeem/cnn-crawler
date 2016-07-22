2893e9df390eb523180f83573aa7225f850d5569
rubhanazeem <rubhanazeem@gmail.com>
2016-07-22 13:39:16 +0500

    tabs added for bootstrap

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,17 @@
 class DashboardController < ApplicationController
   def welcome
+    # Article.fetch_new_articles
+    @new_articles = Article.all
+    @articles = Article.where('tag= ? OR tag= ?', 'Clinton', 'both')
+  end
+
+  def refresh
     Article.fetch_new_articles
     @new_articles = Article.all
+    render template: "welcome"
   end
+
   def clinton
-    @articles = Article.where('tag= ? OR tag= ?', 'Clinton', 'both')
+
   end
 end
