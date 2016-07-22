611348f6ec767fcae488664a8a374502394df164
rubhanazeem <rubhanazeem@gmail.com>
2016-07-22 13:41:16 +0500

    Gem added for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "dashboard#welcome"
   get 'clinton' => 'dashboard#clinton'
+  get 'refresh' => "dashboard#refresh"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
